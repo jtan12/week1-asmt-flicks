2bee8a0f3c82d8a36cd86cd2f8ff1630c57ad49b
JT <JT@jts-mac.local>
2016-02-08 23:56:59 -0500

    Week 2 Setup

---
+++ MovieViewer/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  MovieViewer
+//
+//  Created by JT on 2/8/16.
+//  Copyright © 2016 JT. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
